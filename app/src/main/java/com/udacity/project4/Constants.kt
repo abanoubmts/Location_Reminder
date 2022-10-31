00d8c2cfbf83d38bf9b22219a2e6f6043dc794ba
abanoub.samirfahmi@gmail.com <abanoub.samirfahmi@gmail.com>
2022-10-31 20:56:49 +0200

    Firebase Authentication prepare the requirments and compaiable versions for the project and add google service json file and then apply the below steps : 1 - add layout tag into activity_authentication.xml to enable data binding . 2 - modify activity tag in Mainfest.xml 3 - create new Constants.kt class to include all used constants . 4 - import authentication firebase "import com.firebase.ui auth.AuthUI" / id response classes  and use function called (launchSign) . 5- Create activity result to start another activity and receive a result from firebase result for authentication status . 6- Create startReminderActivity by add intent variable to use start activity func from activity class.

---
+++ app/src/main/java/com/udacity/project4/Constants.kt
@@ -0,0 +1,11 @@
+package com.udacity.project4
+
+import com.google.android.gms.location.Geofence
+
+object Constants {
+
+        // authentication
+        const val TAG_AUTH = "AuthenticationActivity"
+        const val SIGN_IN_RESULT_CODE = 1001
+
+}
--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,19 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.Constants
 import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,6 +21,7 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAuthenticationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
@@ -20,5 +32,67 @@ class AuthenticationActivity : AppCompatActivity() {
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_authentication
+        )
+
+        binding.lifecycleOwner = this
+
+        binding.logInButton.setOnClickListener {
+            launchSign()
+        }
     }
+
+    /* to use AuthUI  / IdpResponse  classes inside firbase package , Firebase  implemented by import this package
+     'com.firebaseui:firebase-ui-auth:7.1.1'  in side gradle project
+
+    */
+    private fun launchSign() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+
+
+        )
+
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            Constants.SIGN_IN_RESULT_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == Constants.SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in
+                Log.i(Constants.TAG_AUTH, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+                Toast.makeText(this, "Successfully signed in", Toast.LENGTH_SHORT).show()
+                startReminderActivity()
+
+            } else {
+
+                // response.getError().getErrorCode() and handle the error.
+                Log.i(Constants.TAG_AUTH, "Sign in failed ${response?.error?.errorCode}")
+                Toast.makeText(this, "Sign in failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startReminderActivity() {
+        val intent = Intent(this, RemindersActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
+
+
+
 }
